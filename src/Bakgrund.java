06cc4b03084c829ed8a5649efb730f6060845620
Tilda Lindblad Aderud <lindbladaderud.tilda@elev.ga.ntig.se>
2023-03-08 13:01:52 +0100

    The panels now arent changeable anymore

--- src/Bakgrund.java
+++ src/Bakgrund.java
@@ -1,19 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 
 public class Bakgrund extends JPanel implements MouseListener {
     //Attributes
-    final int SIZE = 400; //Will be used as size of our window. Big letters because it wont be changed.
+    final int SIZE = 200; //Will be used as size of our window. Big letters because it won't be changed (final).
     private Boolean firstcolor = false; //Will be used to change the color of the background.
     private int number = 0; //Will be used to count how many times clicked.
     private Boolean LR = false; //Will be used to know whether left or right has been clicked.
+    Window myWindow;
+    boolean xo;
 
     //Constructors
-    public Bakgrund() {
+    public Bakgrund(Window window) {
+        myWindow = window;
+        this.xo = window.isXo();
+        this.LR = window.isLeftRight();
         this.setBackground(Color.BLACK); //Sets the color of the background
+        this.setBorder(BorderFactory.createLineBorder(Color.black, 4));
         this.setPreferredSize(new Dimension(SIZE,SIZE)); //Sets preferred size of window
         this.addMouseListener(this); //Adds a mouse listener. The mouse listener will react if something happens to the mouse. (AKA, if anything is clicked)
     }
@@ -27,15 +31,15 @@ public class Bakgrund extends JPanel implements MouseListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g); //Makes g our "paintbrush". AKA what we will use to refer to.
         g.setColor(Color.BLACK); //Sets the color of our "paintbrush" to black
-        if (LR) { //Will draw an "X"
-            g.drawLine((int)(SIZE*0.1),(int)(SIZE*0.1),(int)(SIZE*0.9),(int)(SIZE*0.9));
-            g.drawLine((int)(SIZE*0.9),(int)(SIZE*0.1),(int)(SIZE*0.1),(int)(SIZE*0.9));
-        } else { //Will draw an "O"
-            g.drawOval((int)(SIZE*0.1),(int)(SIZE*0.1),(int)(SIZE*0.8),(int)(SIZE*0.8));
-        }
-        g.setFont(new Font("SansSerif", Font.PLAIN, 50)); //How the text will look
-        g.drawString(String.valueOf(number),SIZE/2-25,SIZE/2); //What text will be printed and where
-        repaint(); //Makes it so that it will update :)
+            if (LR) { //Will draw an "X"
+                g.drawLine((int) (SIZE * 0.1), (int) (SIZE * 0.1), (int) (SIZE * 0.9), (int) (SIZE * 0.9));
+                g.drawLine((int) (SIZE * 0.9), (int) (SIZE * 0.1), (int) (SIZE * 0.1), (int) (SIZE * 0.9));
+            } else { //Will draw an "O"
+                g.drawOval((int) (SIZE * 0.1), (int) (SIZE * 0.1), (int) (SIZE * 0.8), (int) (SIZE * 0.8));
+            }
+            g.setFont(new Font("SansSerif", Font.PLAIN, 50)); //How the text will look
+            g.drawString(String.valueOf(number), SIZE / 2 - 25, SIZE / 2); //What text will be printed and where
+            repaint(); //Makes it so that it will update :)
     }
 
     /*
@@ -43,23 +47,31 @@ public class Bakgrund extends JPanel implements MouseListener {
     */
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (e.getButton() == MouseEvent.BUTTON1) { //If the left button is pressed
-            System.out.println("LEFT");
-            LR = false; //Sets LR to false
+        if (xo) {
+            if (e.getButton() == MouseEvent.BUTTON1) { //If the left button is pressed
+                //System.out.println("LEFT");
+                System.out.println("1");
+                LR = false; //Sets LR to false
+            }
+            if (e.getButton() == MouseEvent.BUTTON1) { //If the right button is pressed
+                //System.out.println("RIGHT");
+                System.out.println("2");
+                LR = true; //Sets LR to true
+            }
+            System.out.println("Mouse clicked! ");
+            number += 1; //Adds 1 to number every time the mouse is clicked.
+            if (myWindow.isBlue()) {
+                this.setBackground(Color.cyan);
+            } else {
+                this.setBackground(Color.YELLOW);
+            }
+            myWindow.setBlue();
+            //firstcolor = !firstcolor; //Changes firstcolor to what it isnt. True->false. False->true
+            repaint(); //Updates
+            System.out.println(xo);
+            xo = !xo;
+            System.out.println(xo);
         }
-        if (e.getButton() == MouseEvent.BUTTON3) { //If the right button is pressed
-            System.out.println("RIGHT");
-            LR = true; //Sets LR to true
-        }
-        System.out.println("Mouse clicked! ");
-        number += 1; //Adds 1 to number every time the mouse is clicked.
-        if (!firstcolor) {
-            this.setBackground(Color.cyan);
-        } else {
-            this.setBackground(Color.YELLOW);
-        }
-        firstcolor = !firstcolor; //Changes firstcolor to what it isnt. True->false. False->true
-        repaint(); //Updates
     }
 
     /*
--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame { //JFrame is the frame of the console. It is in a JFrame everything will happen
     //Attributes
+    private boolean blue = true;
+    private boolean xo = true;
+    private boolean LeftRight = false;
 
     //Concrtuctors
     public Window(){
+
         //Window design and function
         this.setTitle("Color Clicker! :D");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The program will close when the window is closed.
 
+        int row = 3;
+        int col = 3;
+
+        //Create a layout
+        JPanel multipanel = new JPanel();
+        GridLayout layout = new GridLayout(row,col);
+        multipanel.setLayout(layout);
+        multipanel.setBorder(BorderFactory.createLineBorder(Color.black, 10));
+
         //Window content
-        this.add(new Bakgrund());
+        //this.add(new Bakgrund());
+        for (int i = 0; i < row*col; i++) {
+            multipanel.add(new Bakgrund(this)); //There needs to be added same amount of new backgrounds as there are rows*columns.
+        }
+
+        //Add the multipanel to the frame
+        this.getContentPane().add(multipanel);
 
         //Window visible
         this.pack(); //Packages the window.
@@ -19,5 +39,28 @@ public class Window extends JFrame { //JFrame is the frame of the console. It is
 
     //Methods
 
-    //Getters/Setters
+    public boolean isBlue() {
+        return blue;
+    }
+
+    public void setBlue() {
+        this.blue = !this.blue;
+    }
+
+    public boolean isXo() {
+        return xo;
+    }
+
+    public void setXo(boolean xo) {
+        this.xo = xo;
+    }
+
+    public boolean isLeftRight() {
+        LeftRight = !LeftRight;
+        return LeftRight;
+    }
+
+    public void setLeftRight(boolean leftRight) {
+        LeftRight = leftRight;
+    }
 }
